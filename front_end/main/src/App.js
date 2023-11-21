5840c9bd7d390d827ba9710649ff59290ec48ad7
Kummakun <gabriel.blanco@utec.edu.pe>
2023-11-20 23:59:07 -0500

    Front p1

--- front_end/main/src/App.js
+++ front_end/main/src/App.js
@@ -4,22 +4,30 @@ import './App.css';
 import TopBar from './TopBar';
 import SignInForm from './SignInForm';
 import styles from './styles.css';
+import './tarjetas.css'
 
 function Home() {
-  return <h2>Home Page</h2>;
+  return (
+    <div className="container">
+      <div className="tarjeta">Tarjeta 1</div>
+      <div className="tarjeta">Tarjeta 2</div>
+      <div className="tarjeta">Tarjeta 3</div>
+      <div className="tarjeta">Tarjeta 4</div>
+      {/* Agrega más tarjetas según sea necesario */}
+    </div>
+  );
 }
 
 function App() {
   return (
     <Router>
       <div className='body'>
-      <TopBar />
-      <Routes>
-        <Route path="/signin" element={<SignInForm />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <TopBar />
+        <Routes>
+          <Route path="/signin" element={<SignInForm />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-      
     </Router>
   );
 }
