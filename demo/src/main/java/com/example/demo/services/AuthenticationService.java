3e8319a820713084c5c393b1296556945ad80eab
Dezaalonso <adriandezalv@gmail.com>
2023-11-23 02:34:32 -0500

    nuevo log

---
+++ demo/src/main/java/com/example/demo/services/AuthenticationService.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.User;
+import com.example.demo.models.Role;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(String username, String password){
+
+        String encodedPassword = passwordEncoder.encode(password);
+        Role userRole = roleRepository.findByAuthority("USER").get();
+
+        Set<Role> authorities = new HashSet<>();
+
+        authorities.add(userRole);
+
+        return userRepository.save(new User(0, username, encodedPassword, authorities));
+    }
+}
