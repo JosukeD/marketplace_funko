f41b9e00650f116d79fcc6283c29b81f3a4acd0c
Gabriel Romero <afabriziopr@gmail.com>
2023-11-22 22:08:25 -0500

    app movil corregido

--- mobile/my-app/App.js
+++ mobile/my-app/App.js
@@ -1,39 +1,67 @@
 import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import SignInForm from './SignInForm';
+import { View, ImageBackground, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 
+export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-const Stack = createNativeStackNavigator();
-
-const App = () => {
-  const [user, setUser] = useState(null);
-
-  const handleSignIn = (userData) => {
-    setUser(userData);
+  const handleSignIn = () => {
+    console.log('Iniciar sesión con:', username, password);
+    // Aquí puedes realizar lógica adicional para autenticar al usuario
+    
   };
 
-  const handleSignOut = () => {
-    setUser(null);
+  const handleSignUp = () => {
+    console.log('Registrarse con:', username, password);
+    // Aquí puedes realizar lógica adicional para crear una nueva cuenta de usuario
   };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={user ? 'Home' : 'SignIn'}>
-        <Stack.Screen
-          name="Home"
-          component={() => <HomeScreen user={user} onSignOut={handleSignOut} />}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignIn"
-          component={() => <SignInForm onSignIn={handleSignIn} />}
-          options={{ title: 'Sign In' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+    <View style={{ flex: 1 }}>
+      {/* Encabezado */}
+      <View style={{ height: '15%', backgroundColor: 'rgb(3, 30, 60)', flexDirection: 'row', padding: 10, alignItems: 'flex-end' }}>
+        <Image source={require('./logo.jpg')} style={{ width: 60, height: 60 }} />
+        <Text style={{ fontWeight: 'bold', fontSize: 20, marginLeft: 10, color: 'white', alignSelf: 'center', marginTop: '9%' }}>MARKETPLACE FUNKOS</Text>
+      </View>
 
-export default App;
+      {/* Fondo con recuadro central */}
+      <ImageBackground source={require('./funkobackground.jpg')} style={{ flex: 1, resizeMode: 'cover' }}>
+        {/* Recuadro central */}
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+          {/* Componente de formulario */}
+          <View style={{ width: '80%' }}>
+            {/* Entrada de usuario */}
+            <TextInput
+              placeholder="Usuario"
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+              style={{ backgroundColor: 'white', marginBottom: 10, padding: 10, borderRadius: 5 }}
+            />
+
+            {/* Entrada de contraseña */}
+            <TextInput
+              placeholder="Contraseña"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+              style={{ backgroundColor: 'white', marginBottom: 10, padding: 10, borderRadius: 5 }}
+            />
+
+            {/* Botones de inicio de sesión y registro */}
+            <TouchableOpacity
+              style={{ backgroundColor: 'rgb(3, 30, 60)', padding: 10, borderRadius: 5, alignItems: 'center' }}
+              onPress={handleSignIn}>
+              <Text style={{ color: 'white' }}>Iniciar sesión</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{ backgroundColor: 'rgb(3, 30, 60)', padding: 10, borderRadius: 5, alignItems: 'center', marginTop: 10 }}
+              onPress={handleSignUp}>
+              <Text style={{ color: 'white' }}>Registrarse</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
